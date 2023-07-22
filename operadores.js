cbef2767973f72019201e7f04adf89662ca7adf1
rogeramosh <rogeramosh@outlook.es>
2023-07-22 13:16:33 -0500

    Bueno

--- operadores.js
+++ operadores.js
@@ -2,4 +2,10 @@ console.log(3 + 5);
 console.log(17-6);
 console.log(5*6);
 console.log(30/7);
-console.log(30%4)
+console.log(30%4);
+//Reglas de precedencia
+console.log(3+5*2-8);
+console.log("hola " + "Camilo");
+var nombre= "Camilo"
+var saludo = "Hola "+ nombre;
+console.log(saludo);
