23178d94ef225efacbc777fdb9b75c9f3b4bc963
rogeramosh <rogeramosh@outlook.es>
2023-07-22 14:39:42 -0500

    22.07.2023 14:40

---
+++ OperadoresComparacion.js
@@ -0,0 +1,5 @@
+console.log(4<7);
+console.log(4<1);
+console.log(4>4);
+console.log(4==7);
+console.log(4!=5);
--- operadores.js
+++ operadores.js
@@ -3,9 +3,14 @@ console.log(17-6);
 console.log(5*6);
 console.log(30/7);
 console.log(30%4);
+console.log(2**5);
 //Reglas de precedencia
 console.log(3+5*2-8);
 console.log("hola " + "Camilo");
 var nombre= "Camilo"
 var saludo = "Hola "+ nombre;
 console.log(saludo);
+var a =1;
+var b = 2;
+var c= (a=b);
+console.log("a:"+a+" b:"+b+" c:"+c);
