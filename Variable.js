fafed88985bf5aa85d1e544768c68a147032ef09
rogeramosh <rogeramosh@outlook.es>
2023-07-22 11:13:51 -0500

    new curso repositorio

---
+++ Variable.js
@@ -0,0 +1,16 @@
+
+
+//String
+var bootcamp = "Henry";
+
+// Numeros
+var NumeroEntero = 17;
+var NumeroDecimal = 1.77;
+var NumeroNegativo= -20;
+//idefinida
+var cajon;
+console.log(cajon);
+//Null
+var casa= null;
+
+
