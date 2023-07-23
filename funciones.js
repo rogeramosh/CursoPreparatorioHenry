6cef06c319f7796bdcce6aa8b8f732326b79d629
rogeramosh <rogeramosh@outlook.es>
2023-07-22 20:02:40 -0500

    Proximo 8:02 Proximo

---
+++ funciones.js
@@ -0,0 +1,22 @@
+function sumaTres(x){
+    console.log(x+3);
+}
+
+sumaTres(5);
+//Primero
+function sumaTres1(x){
+    return x+3;
+}
+console.log(sumaTres1(3))
+//Segundo
+
+var SumaTres2= function(x){
+    return x+3;
+}
+console.log(SumaTres2(7))
+ //Tercera
+ var SumaTres3 = (x) => {
+    return x+3;
+ }
+ console.log(SumaTres3(12))
+
---
+++ funciones2.js
@@ -0,0 +1,15 @@
+function CuidadoConelConsoleLOg(nombre){
+    console.log(nombre);
+    return nombre
+}
+CuidadoConelConsoleLOg('Roger');
+function  otrsFuncion(){
+    return("Retorne el valor de la función 'CuidadoConelConsoleLOg'  es"  + CuidadoConelConsoleLOg('Roger'))
+}
+otrsFuncion();
+
+function CuidadoConElReturn(nombre){
+    console.log(nombre);
+    return nombre;
+}
+ 
