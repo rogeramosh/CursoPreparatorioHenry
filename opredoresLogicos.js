2a004ad20431ee5a5725c5a445fdd86dc7de4e65
rogeramosh <rogeramosh@outlook.es>
2023-07-24 15:18:22 -0500

    new 24.07.2023 03:18pm

---
+++ opredoresLogicos.js
@@ -0,0 +1,13 @@
+function  mayorYmenor(num){
+    if (num>2 &&num<20)console.log(true); else console.log(false);
+        
+    
+}
+mayorYmenor(10);
+mayorYmenor(25);
+
+
+function mayorYmenorYpar(num){
+    if(num>100 && num<200 && num % 2 ===0) console.log(true); else console.log(false);
+}
+mayorYmenorYpar(90);
