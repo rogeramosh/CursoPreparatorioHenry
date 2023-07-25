b51d191cbc6f93c2963aeedbe960197bed1ba8a4
rogeramosh <rogeramosh@outlook.es>
2023-07-25 05:39:59 -0500

    25.07.2023  5:40am

---
+++ bucles.js
@@ -0,0 +1,16 @@
+var suma=0
+for(var i=0;i<= 10; i++){
+   suma=suma+1
+    console.log("suma 1 en 1="+ suma);
+}
+
+suma=0;
+
+for(var i=0;i<= 10; i++){
+   suma=suma+i
+    console.log("suma="+ suma);
+}
+
+
+// While
+
