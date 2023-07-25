800590c0151893ea8d92cabcb0156b826994afa0
rogeramosh <rogeramosh@outlook.es>
2023-07-25 00:04:09 -0500

    25.07.2023 12:04

--- opredoresLogicos.js
+++ opredoresLogicos.js
@@ -1,3 +1,6 @@
+
+//&& CONJINCIÓN Y >MAYOR QUE, < MENOR QUE
+
 function  mayorYmenor(num){
     if (num>2 &&num<20)console.log(true); else console.log(false);
         
@@ -6,8 +9,23 @@ function  mayorYmenor(num){
 mayorYmenor(10);
 mayorYmenor(25);
 
-
+//&&, CONJUNCIÓN
 function mayorYmenorYpar(num){
     if(num>100 && num<200 && num % 2 ===0) console.log(true); else console.log(false);
 }
 mayorYmenorYpar(90);
+
+//OR DISYUNCIÓN
+
+function operadorOr(str){
+    if (str === "Henry" || str.length<2) console.log(true); else console.log(false);
+}
+operadorOr("n");
+
+//NEGACIÓN
+function negacion(permiso){
+    if(permiso !== false)console.log("Tiene permiso");else console.log("No tiene permiso");
+}
+negacion(false);
+
+
